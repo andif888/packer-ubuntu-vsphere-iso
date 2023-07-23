44f81c73f3d4ea8654dd40bb07796b2736d4ed43
Javel Palmer <37094748+j4v3l@users.noreply.github.com>
2023-07-23 00:56:23 -0400

    Update build-2204.sh to download ISO directly from ubuntu
    The current link is dead and it was from a unsecured server (HTTP and not HTTPS)

--- build-2204.sh
+++ build-2204.sh
@@ -2,6 +2,6 @@
 # variable files ending with .auto.pkrvars.hcl are automatically loaded
 packer build -var-file="ubuntu-2204.pkrvars.hcl" \
   -var='os_iso_checksum=84aeaf7823c8c61baa0ae862d0a06b03409394800000b3235854a6b38eb4856f' \
-  -var='os_iso_url=http://ftp.halifax.rwth-aachen.de/ubuntu-releases/jammy/ubuntu-22.04-live-server-amd64.iso' \
+  -var='os_iso_url=https://old-releases.ubuntu.com/releases/22.04/ubuntu-22.04-live-server-amd64.iso' \
   -var='vsphere_guest_os_type=ubuntu64Guest' \
   -var='vsphere_vm_name=tpl-ubuntu-2204' .
